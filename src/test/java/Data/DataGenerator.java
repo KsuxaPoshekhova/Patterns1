d30f20b429bfe9f7cc88cb2a13469c6b1c43558a
Ksenia Poshekhova <kposehova@gmail.com>
2024-03-17 10:24:31 +0300

    Inichial commit

---
+++ src/test/java/Data/DataGenerator.java
@@ -0,0 +1,50 @@
+package Data;
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateData(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[] {"Астрахань", "Кострома","Краснодар","Москва",
+                "Петропавловск-Камчатский","Ростов-на-Дону","Салехард","Самара",
+                "Санкт-Петербург","Саранск","Саратов","Севастополь","Ставрополь",
+                "Сыктывкар","Чебоксары","Элиста","Южно-Сахалинск","Ярославль"};
+        return cities [new Random().nextInt(cities.length)];
+    }
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+    public static String generatePhone(String lokale) {
+        var faker = new Faker(new Locale(lokale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+
+        }
+
+        public static userInfo  generateUser(String locale) {
+            return new userInfo(generateCity(), generatePhone(locale), generateName(locale)) ;
+        }
+    }
+
+    @Value
+    public static class userInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,61 @@
+package ru.netology;
+
+import Data.DataGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CardDeliveryTest {
+
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-sha-usage");
+        options.addArguments("--no-sandbox ");
+        options.addArguments("--headless ");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Card with delivery")
+    void meetingPlanTest() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        int  appointmentDate = 3;
+        String  firstMeetingDate = DataGenerator.generateData(appointmentDate);
+        int  newAppointmentDate = 4;
+        String  daysToAddForSecondMeeting = DataGenerator.generateData(newAppointmentDate);
+        $("[date-test-id='city'] input").setValue(validUser.getCity());
+        $("[date-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[date-test-id='date'] input").setValue(firstMeetingDate);
+        $("[date-test-id='name'] input").setValue(validUser.getName());
+        $("[date-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[date-test-id='agreement']").click();
+        $("button").click();
+        $(byText("Успешно запланированна ")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification']. notification")
+                .shouldHave(exactText("Встреча успешно запланированна на " + appointmentDate ))
+                .shouldBe(visible);
+        $("[date-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[date-test-id='date'] input").setValue(firstMeetingDate);
+        $("button").click();
+        $("[date-test-id='replan-notification'].notification_content")
+                .shouldHave(exactText("У вас уже запланированна встреча на другую дату.Перепланировать? "))
+                .shouldBe(visible);
+        $("[date-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification']. notification")
+                .shouldHave(exactText("Встреча успешно запланированна на " + daysToAddForSecondMeeting ))
+                .shouldBe(visible);
+    }
+}
